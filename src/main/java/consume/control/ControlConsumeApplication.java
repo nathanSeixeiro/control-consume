0ef025034c723fc2061550caf6dc503852a9357d
Luiz Fernando Amador <luizfernando46464@gmail.com>
2023-11-27 20:18:47 -0300

    initial commit

---
+++ src/main/java/consume/control/ControlConsumeApplication.java
@@ -0,0 +1,13 @@
+package consume.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlConsumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlConsumeApplication.class, args);
+	}
+
+}
